5d5b30c1491d8c21b353f13d2e5ef9b7deee6d26
dr33k <dr33k@users.noreply.com>
2022-04-11 14:57:14 +0100

    Switched from Basic Authentication to Form Based Authentication

--- src/main/java/com/agicomputers/LoanAPI/controllers/TemplateController.java
+++ src/main/java/com/agicomputers/LoanAPI/controllers/TemplateController.java
@@ -1,4 +1,21 @@
 package com.agicomputers.LoanAPI.controllers;
 
-public class TemplateController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController  {
+
+    @GetMapping("login")
+    public String getLoginPage(){
+        return "login";
+    }
+
+
+    @GetMapping("customers")
+    public String getCustomersPage(){
+        return "customers";
+    }
 }
--- src/main/java/com/agicomputers/LoanAPI/security/AppSecurityConfig.java
+++ src/main/java/com/agicomputers/LoanAPI/security/AppSecurityConfig.java
@@ -15,8 +15,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
 @EnableWebSecurity
